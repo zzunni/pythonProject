ffe6c084cbcd5afcb4204ad0a48f204d3c08ca39
zzunni <12201309@inha.edu>
2024-01-16 17:40:36 +0900

    ãday03 practice problem

---
+++ day03_practice.py
@@ -0,0 +1,11 @@
+# 6.1 for문으로 리스트 [3,2,1,0] 출력
+
+num=[]
+for i in range(3,-1,-1):
+    num.append(i)
+print(num)
+
+# 6.2 guess_me, number를 비교하는 while 문 작성
+
+
+# 6.3 guess_me, number를 비교하는 for 문 작성
