0093324260616046a7d87b51ecf0df6911344a5e
zzunni <12201309@inha.edu>
2024-01-19 15:10:29 +0900

    day06_02

--- day06.py
+++ day06.py
@@ -1,7 +1,21 @@
+class FlyingMixin:
+    def fly(self):
+        return f"{self.name}이(가) 하늘을 훨훨 날아갑니다~"
+
+class SwimmingMixin:
+    def swim(self):
+        return f"{self.name}이(가) 수영을 합니다."
 class Poketmon:
     def __init__(self, name):   #생성자(객체가 생성될 때 딱 한번 실행)
-        print(f"{name} 포켓몬스터 생성")
+        self.name = name
 
+class Charizard(Poketmon,FlyingMixin):
+    pass
 
-pikachu = Poketmon("피카츄")
-squirtle = Poketmon("꼬부기")
+class Gyarados(Poketmon,SwimmingMixin):
+    pass
+
+g1 = Gyarados("갸라도스")
+c1 = Charizard("리쟈몽")
+print(c1.fly())
+print(g1.swim())
